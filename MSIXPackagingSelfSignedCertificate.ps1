99831c3c082306c8491033f3001b82b293f8a27d
YenalT <100377515+YenalT@users.noreply.github.com>
2025-02-26 10:02:17 +0300

    Add files via upload

---
+++ MSIXPackagingSelfSignedCertificate.ps1
@@ -0,0 +1,4 @@
+﻿New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -Subject "CN=MSIXPackagingCert" -Type CodeSigningCert -KeyAlgorithm RSA -KeyLength 2048 -HashAlgorithm SHA256 -NotAfter (Get-Date).AddYears(2) -FriendlyName "MSIX Packaging Signing Certificate"
+$cert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=MSIXPackagingCert" }
+$certPassword = ConvertTo-SecureString -String "Parola" -Force -AsPlainText
+Export-PfxCertificate -Cert $cert -FilePath "C:\cert\MSIXPackagingCert.pfx" -Password $certPassword
